54f344eaf265d1da31fdd58205322cd28eac5163
Itzac Angelo Albertin <itzacalbertin@gmail.com>
2024-10-23 08:49:51 -0300

    initial commit

---
+++ src/main/java/com/example/unika_backend/UnikaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.unika_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnikaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unika_backend/UnikaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unika_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnikaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
